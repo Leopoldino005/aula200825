94be64370e712b78bd53ec8007b0253288b31c34
Leopoldino005 <gabrielleopoldino073@gmail.com>
2025-08-20 20:12:59 -0300

    atualização

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,16 +1,8 @@
-﻿byte idade;
-string nome;
-Console.Write("Digite seu nome: ");
-nome = Console.ReadLine();
-Console.Write("Digite sua idade: ");
-idade = Convert.ToByte(Console.ReadLine());
-Console.WriteLine("Olá " + nome + ". A idade informada foi {0}", idade);// idade = 20
-idade *= 2; // idade = idade * 2 
-Console.WriteLine($"A nova idade é {idade++}"); //40
-Console.WriteLine($"A nova idade é {++idade}"); //42
+﻿decimal salario, irpfDevido;
+Console.Write("Informe seu salário: ");
+salario = decimal.Parse(Console.ReadLine());
 
-//altere o programa para que solicite ao usuário informar o seu salário e ao final informe o desconto de IRPF de acordo com o salário informado. Utilize a tabela oficial de descontos de IRPF do Brasil.
-
-
-
-
+if (salario <= 2428.8m)
+    irpfDevido = 0;
+else if (salario <= 2826.65m)
+    irpfDevido = salario * 0.075m
